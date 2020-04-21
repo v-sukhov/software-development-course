17cab6a431fe5340bc5e685162ef5f7de1277238
MaxanRus <timosha1113@yandex.ru>
2020-04-22 00:29:14 +0300

    inital commit

---
+++ tcp-client-server/01-simple-client-server/Timofey/client.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  cout << "I'm the client :)" << endl;
+
+  return 0;
+}
---
+++ tcp-client-server/01-simple-client-server/Timofey/server.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+  cout << "I'm the server" << endl;
+
+  return 0;
+}
