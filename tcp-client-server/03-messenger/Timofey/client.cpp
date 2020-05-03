41df28baf25bbbe30a48f0bdba8eb625f03f5cde
MaxanRus <timosha1113@yandex.ru>
2020-05-03 03:49:57 +0300

    Receiving messages

--- tcp-client-server/03-messenger/Timofey/client.cpp
+++ tcp-client-server/03-messenger/Timofey/client.cpp
@@ -12,6 +12,14 @@
 
 WINDOW *chat, *input;
 
+void printMessage(char *msg) {
+	int x, y;
+	getyx(chat, y, x);
+	wprintw(chat, "%s", msg);
+	wmove(chat, y + 1, x);
+	wrefresh(chat);
+}
+
 int main(int argc, char *argv[]) {
 	int sockfd = 0;
 	struct sockaddr_in serv_addr;
@@ -69,15 +77,47 @@ int main(int argc, char *argv[]) {
 	box(chat, 0, 0);
 	wmove(chat, 0, 1);
 	wprintw(chat, "chat");
+	wmove(chat, 1, 2);
 	
 	input = newwin(1, COLS, LINES - 1, 0);
 	wprintw(input, "%s > ", name);
 
 	wrefresh(chat);
 	wrefresh(input);
-	
-    getch();
 
+	printMessage("help");
+
+	fd_set rfds;
+	FD_ZERO(&rfds);
+
+	char recBuff[1025];
+	noecho();
+
+	
+	int x, y;
+	getyx(input, y, x);
+	wmove(input, y, x);
+	wrefresh(input);
+
+	while (true) {
+		FD_SET(0, &rfds);	
+		FD_SET(sockfd, &rfds);
+
+		select(sockfd + 1, &rfds, NULL, NULL, NULL);
+
+		if (FD_ISSET(0, &rfds)) {
+			break;
+		}
+		if (FD_ISSET(sockfd, &rfds)) {
+			int n = read(sockfd, recBuff, 1024);
+			recBuff[n] = 0;
+			printMessage(recBuff);
+		}
+		int x, y;
+		getyx(input, y, x);
+		wmove(input, y, x);
+		wrefresh(input);
+	}
     delwin(chat);
 	delwin(input);
     endwin();
